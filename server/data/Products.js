bfa114a99eeebffe77ace8b38f764eb3839d77d3
9inelife <preshomezi@gmail.com>
2022-08-16 15:29:41 +0100

    shop

---
+++ server/data/Products.js
@@ -0,0 +1,185 @@
+const products = [ 
+  {
+    name: "Banana",
+    image: "https://res.cloudinary.com/presh9/image/upload/v1651148029/bananas_qjqjga.jpg",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
+    price: 89,
+    countInStock: 3,
+    rating: 4,
+    numReviews: 4,
+  },
+  {
+    name: "Plaintain",
+    image: "https://res.cloudinary.com/presh9/image/upload/v1651148029/plantains_oovkhq.jpg",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
+    price: 89,
+    countInStock: 3,
+    rating: 4,
+    numReviews: 4,
+  },
+  {
+    name: "Coconut",
+    image: "https://res.cloudinary.com/presh9/image/upload/v1651148029/coconuts_yrwyex.jpg",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
+    price: 89,
+    countInStock: 3,
+    rating: 4,
+    numReviews: 4,
+  },
+  {
+    name: "Yam",
+    image: "https://res.cloudinary.com/presh9/image/upload/v1651147532/yam_zykcsb.jpg",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
+    price: 89,
+    countInStock: 3,
+    rating: 4,
+    numReviews: 4,
+  },
+  {
+    name: "Corn",
+    image: "https://res.cloudinary.com/presh9/image/upload/v1651147531/corn_rx9dwi.jpg",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
+    price: 89,
+    countInStock: 3,
+    rating: 4,
+    numReviews: 4,
+  },
+  {
+    name: "Potatoes",
+    image: "https://res.cloudinary.com/presh9/image/upload/v1651147531/potato_njvo9g.jpg",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
+    price: 89,
+    countInStock: 3,
+    rating: 4,
+    numReviews: 4,
+  },
+  {
+    name: "Bag of Onions",
+    image: "https://res.cloudinary.com/presh9/image/upload/v1651147530/onion_lulqjt.jpg",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
+    price: 89,
+    countInStock: 3,
+    rating: 4,
+    numReviews: 4,
+  },
+  {
+    name: "watermelon",
+    image: "https://res.cloudinary.com/presh9/image/upload/v1651147531/watermelon_hycyhk.jpg",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
+    price: 89,
+    countInStock: 3,
+    rating: 4,
+    numReviews: 4,
+  },
+  {
+    name: "Cocoa",
+    image: "https://res.cloudinary.com/presh9/image/upload/v1651147531/cocoa_khk4q7.jpg",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
+    price: 89,
+    countInStock: 3,
+    rating: 4,
+    numReviews: 4,
+  },
+  {
+    name: "Cassava",
+    image: "https://res.cloudinary.com/presh9/image/upload/v1651147529/cassva_v0fpgr.jpg",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
+    price: 89,
+    countInStock: 3,
+    rating: 4,
+    numReviews: 4,
+  },
+  {
+  name: "Oranges",
+  image: "https://res.cloudinary.com/presh9/image/upload/v1651147529/oranges_ktauua.jpg",
+  description:
+    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
+  price: 89,
+  countInStock: 3,
+  rating: 4,
+  numReviews: 4,
+},
+
+  {
+    name: "Rice Bag",
+    image: "https://res.cloudinary.com/presh9/image/upload/v1651147530/Rice_jzqmoq.jpg",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
+    price: 89,
+    countInStock: 3,
+    rating: 4,
+    numReviews: 4,
+  },
+  {
+    name: "Cocoa Yam",
+    image: "https://res.cloudinary.com/presh9/image/upload/v1651147529/cocoyam_wuvwmt.jpg",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
+    price: 89,
+    countInStock: 3,
+    rating: 4,
+    numReviews: 4,
+  },
+  {
+    name: "Garri",
+    image: "https://res.cloudinary.com/presh9/image/upload/v1651147529/garri_kr3geu.jpg",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
+    price: 599,
+    countInStock: 10,
+    rating: 2,
+    numReviews: 2,
+  },
+  {
+    name: "Beans Bag",
+    image: "https://res.cloudinary.com/presh9/image/upload/v1651147529/beans_mqhv8j.jpg",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
+    price: 929,
+    countInStock: 0,
+    rating: 3.5,
+    numReviews: 3,
+  },
+  {
+    name: "Lace Casual Boots For Boys & Girls  (Tan)",
+    image: "/images/3.png",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
+    price: 399,
+    countInStock: 10,
+    rating: 5,
+    numReviews: 9,
+  },
+  {
+    name: "Lace Walking Shoes For Boys & Girls  (Pink)",
+    image: "/images/2.png",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
+    price: 49,
+    countInStock: 7,
+    rating: 2,
+    numReviews: 2,
+  },
+  {
+    name: "Women Red Heels Sandal",
+    image: "/images/1.png",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
+    price: 29,
+    countInStock: 0,
+    rating: 0,
+    numReviews: 0,
+  },
+];
+
+export default products;
