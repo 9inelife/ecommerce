7f66592bcf9ae1e4596c566b3527ee30bb2828e9
9inelife <85375394+9inelife@users.noreply.github.com>
2022-10-27 18:34:48 +0100

    Update index.js

--- clientfrontend/src/index.js
+++ clientfrontend/src/index.js
@@ -4,18 +4,18 @@ import App from "./App";
 import store from "./Redux/store";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
-import { MoralisProvider } from "react-moralis";
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <MoralisProvider appId="J8XLBQ1cVv0s1if54n1kK9cwRQk4adeeuJnHQG5t" serverUrl="https://gkglgfw1sxxx.usemoralis.com:2053/server">
-      <BrowserRouter>
-      <Provider store={store}>
-        <App />
-        </Provider>
-      </BrowserRouter>
-    </MoralisProvider>
-  </React.StrictMode>,
-  
+ 
+    <BrowserRouter>
+    <Provider store={store}>
+      <App />
+      </Provider>
+    </BrowserRouter>
+ 
+</React.StrictMode>,
+
   document.getElementById("root")
 );
